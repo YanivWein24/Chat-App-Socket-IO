67a8a296ff4f9d9a858005c678118c93d37a633f
YanivWein24 <yanivwein22@gmail.com>
2022-09-19 22:14:47 +0300

    Added option to join a room on 'Enter'

--- client/src/screens/Join/Join.js
+++ client/src/screens/Join/Join.js
@@ -13,13 +13,19 @@ const Join = () => {
                 <h1 className="heading">Join <i className="fa-solid fa-arrow-right-to-bracket"></i></h1>
                 {room && <p style={{ color: "white" }}>Joining Room: {room} &ensp; {name && `As: ${name}`}</p>}
                 <div>
-                    <input placeholder="Room" className="joinInput" type="text" onChange={(e) => setRoom(e.target.value)} />
+                    <input placeholder="Room" className="joinInput" type="text" onKeyUp={(event) =>
+                        event.key !== "Enter" ?
+                            setRoom(event.target.value) :
+                            (name !== '' && room !== '') && document.getElementsByTagName('button')[0].click()} />
                 </div>
                 <div>
-                    <input placeholder="Name" className="joinInput mt-20" type="text" onChange={(e) => setName(e.target.value)} />
+                    <input placeholder="Name" className="joinInput mt-20" type="text" onKeyUp={(event) =>
+                        event.key === "Enter" ?
+                            (name !== '' && room !== '') && document.getElementsByTagName('button')[0].click() :
+                            setName(event.target.value)} />
                 </div>
                 {/* if there is no value for 'name' or 'room', prevent default and stay in the same route, else continue as usual  */}
-                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                     <button className='button'>Sign In</button>
                 </Link>
             </div>
